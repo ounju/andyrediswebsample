ccdb82a8c6bde7c56936d95f44507319d4456eb6
ounju <kimounju@gmail.com>
2019-12-10 19:35:47 +0900

    init
    init

---
+++ src/main/java/sre/andyredis/Andy.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sre.andyredis;
+
+/**
+ *
+ * @author ounju-kim
+ */
+public class Andy {
+
+    public Andy() {
+        System.out.println("aaazzzzzzzzzzzzzzzzzzzzzzzz");
+    }
+ 
+    public static void main(String args[]){
+        new Andy();
+    }
+    
+}
